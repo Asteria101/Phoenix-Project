395d6bfb39d50d9dd7e9d2225df0c017310d7262
Asteria <lebrito1268@gmail.com>
2024-01-17 10:21:58 -0300

    update

---
+++ main.py
@@ -0,0 +1,11 @@
+import player
+import threading
+
+def main():
+    player1 = player.Player()
+    player2 = player.Player("Alfredo", 13, (1,0))
+
+    #TH = threading.Thread(target=)
+
+if __name__ == "__main__":
+    main()
--- player.py
+++ player.py
@@ -1,16 +1,21 @@
 from behaviour import Behaviour
 
 class Player():
-    def __init__(self, name : str, number : int, position : tuple[int]) -> None:
-        self.name : str = "Leticia"
-        self.number : int = 21
-        self.position : tuple[int] = (0, 0)
+    def __init__(self, name : str = "Leticia", number : int = 21, position : tuple[int] = (0, 0)) -> None:
+        self.name : str = name
+        self.number : int = number
+        self.position : tuple[int] = position
         self.ball_own = Behaviour.ball_owner
 
-        print(f"A jogadora {self.name}, cujo número é {self.number}, está na posição {self.position}.")
+        self.print_intro()
 
     def kick(self, distance_ball : float):
         if distance_ball <= 0.6 and self.ball_own:
             print("Dá pra chutar a bola.")
 
-        
+    def print_intro(self):
+        if self.name == "Leticia":
+            print(f"A jogadora {self.name}, cujo número é {self.number}, está na posição {self.position}.")
+        else:
+            print(f"O jogador {self.name}, cujo número é {self.number}, está na posição {self.position}.")
+            
