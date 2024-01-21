c5d7d0210f751df2b167f5646a0c8665fbeb1b81
Asteria <lebrito1268@gmail.com>
2024-01-21 10:50:43 -0300

    add

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ def main():
     player1 = player.Player()
     player2 = player.Player("Alfredo", 13, (1,0))
 
-    player1.run()
-
-    #TH = threading.Thread(target=)
+    TH = threading.Thread(target=player1)
 
 if __name__ == "__main__":
     main()
