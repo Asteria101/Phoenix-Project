740fd4f217dba63f065fc98f47daf338483965ea
Asteria <lebrito1268@gmail.com>
2024-01-26 21:36:42 -0300

    remove thread val

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import player
-import threading
 
 def main():
     player1 = player.Player()
     player2 = player.Player("Alfredo", 13, (1,0))
-
-    TH = threading.Thread(target=player1)
-    TH.start()
 
 if __name__ == "__main__":
     main()
