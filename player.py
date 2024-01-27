17daf3b05a8e914754acaed4c8f734cce4ecbacf
Asteria <lebrito1268@gmail.com>
2024-01-26 21:37:12 -0300

    fix behaviour call

--- player.py
+++ player.py
@@ -1,24 +1,35 @@
 from behaviour import Behaviour
+import threading
 
 class Player():
     def __init__(self, name : str = "Leticia", number : int = 21, position : tuple[int] = (0, 0)) -> None:
         self.name : str = name
         self.number : int = number
         self.position : tuple[int] = position
-        self.behaviour = Behaviour.ball_owner()
+        self.behaviour = Behaviour()
 
         self.print_intro()
+        
+        self.target = threading.Thread(target=self.get_info)
+        self.target.start()
 
     def kick(self, distance_ball : float):
-        if distance_ball <= 0.6 and self.behaviour:
+        if distance_ball <= 0.6 and self.behaviour.ball_owner():
             print("Dá pra chutar a bola.")
 
     def run(self):
-        print(f"Estou correndo.")
-
+        print(f"Estou correndo. E estou {self.behaviour.mood}")
+        
     def print_intro(self):
         if self.name == "Leticia":
-            print(f"A jogadora {self.name}, cujo número é {self.number}, está na posição {self.position}.")
+            print(f"A jogadora {self.name}", end="")
         else:
-            print(f"O jogador {self.name}, cujo número é {self.number}, está na posição {self.position}.")
+            print(f"O jogador {self.name}", end="")
+
+        print(f", cujo número é {self.number}, está na posição {self.position}.")
+
+    def get_info(self):
+        while True:
+            info = []
+            info.append(self.run)
             
